58e072477549e87211457f1b27e64f4392a82b99
dimastrik-78 <78548398+dimastrik-78@users.noreply.github.com>
2023-03-30 17:19:43 +0300

    flipper

--- Game/Assets/_Source/StickSystem/Stick.cs
+++ Game/Assets/_Source/StickSystem/Stick.cs
@@ -4,37 +4,36 @@ using UnityEngine;
 
 public class Stick : MonoBehaviour
 {
-    [SerializeField] private Rigidbody RightStick;
-    [SerializeField] private Rigidbody LeftStick;
+    [SerializeField] private HingeJoint RightStick;
+    [SerializeField] private HingeJoint LeftStick;
 
     private Play _pl;
 
     void Awake()
     {
         _pl = new Play();
-        _pl.Stick.Right.performed += contex => ActionRight();
-        _pl.Stick.Left.performed += contex => ActionLeft();
+        _pl.Stick.Right.performed += key => ActionRight();
+        _pl.Stick.Left.performed += key => ActionLeft();
         _pl.Enable();
     }
 
     private void ActionRight()
     {
-        StartCoroutine(Rotation(RightStick, RightStick.rotation, Quaternion.Euler(15, 0, -45f)));
+        StartCoroutine(Rotation(RightStick));
     }
 
     private void ActionLeft()
     {
-        StartCoroutine(Rotation(LeftStick, LeftStick.rotation, Quaternion.Euler(15, 0, 45f)));
+        StartCoroutine(Rotation(LeftStick));
+
     }
 
-    private IEnumerator Rotation(Rigidbody target, Quaternion startPosition, Quaternion endPosition)
+    private IEnumerator Rotation(HingeJoint joint)
     {
-        //target.rotation = endPosition;
-        target.rotation = Quaternion.Lerp(target.rotation, endPosition, 5f);
+        joint.useMotor = true;
 
         yield return new WaitForSeconds(0.1f);
-        
-        //target.rotation = startPosition;
-        //target.rotation = Quaternion.Lerp(target.rotation, startPosition, 5f);
+
+        joint.useMotor = false;
     }
 }
